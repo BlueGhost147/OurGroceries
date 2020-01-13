80d3f2877e0074f90f95636888263dc8f4e187bc
meistern17 <nadine.meister@edu.fh-joanneum.at>
2020-01-13 17:38:08 +0100

    Custom validator of email in registration

--- frontend/our-groceries/src/app/app.module.ts
+++ frontend/our-groceries/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ItemListComponent } from './item-list/item-list.component';
 import { SingleItemComponent } from './single-item/single-item.component';
 import { HomeOverviewComponent } from './home-overview/home-overview.component';
 import { ListFormComponent } from './list-form/list-form.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {JwtModule} from '@auth0/angular-jwt';
@@ -27,14 +27,15 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatDialogModule} from '@angular/material/dialog';
 import { LockScreenComponent } from './lock-screen/lock-screen.component';
-import {MatIconModule, MatTabsModule} from "@angular/material";
-import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule, MatTabsModule} from '@angular/material';
+import {MatExpansionModule} from '@angular/material/expansion';
 import { ItemDialogComponent } from './item-dialog/item-dialog.component';
-import {DragDropModule} from "@angular/cdk/drag-drop";
-import {DatePipe} from "@angular/common";
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {DatePipe} from '@angular/common';
+import {HttperrorInterceptor} from './httperror.interceptor';
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
@@ -90,7 +91,12 @@ export function tokenGetter() {
     MatIconModule,
     DragDropModule,
   ],
-  providers: [DatePipe],
+  providers: [DatePipe, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttperrorInterceptor,
+    multi: true,
+    deps: [MatSnackBar]
+  }],
   bootstrap: [AppComponent],
   entryComponents: [SingleItemComponent, ItemListComponent, ItemDialogComponent],
 })
--- frontend/our-groceries/src/app/register/register.component.ts
+++ frontend/our-groceries/src/app/register/register.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
-import {MatSnackBar} from "@angular/material";
-import {HttpClient} from "@angular/common/http";
-import {UserService} from "../services/user.service";
-import {BehaviorSubject} from "rxjs/internal/BehaviorSubject";
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
+import {HttpClient} from '@angular/common/http';
+import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -15,38 +14,59 @@ export class RegisterComponent implements OnInit {
 
   registerFormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router, private userService: UserService, private _snackBar: MatSnackBar) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router,
+              private userService: UserService, private snackBar: MatSnackBar) {
   }
 
   ngOnInit() {
-    if (this.userService.isLoggedIn.getValue() === true)
+    if (this.userService.isLoggedIn.getValue() === true) {
       this.router.navigate(['home']);
+    }
 
     this.registerFormGroup = this.fb.group({
       username: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['', [this.emailValidator(), Validators.required]],
       password: ['', Validators.required],
     });
   }
 
   register() {
-    this.http.post('/api/register/', this.registerFormGroup.value)
-      .subscribe((res: any) => {
-        this._snackBar.open('New user registered ', null, {
-          duration: 2000,
-        });
-        this.ngOnInit();
-      }, (error) => {
-        let msg = 'Registration failed! ';
+    if (this.registerFormGroup.valid) {
+      this.http.post('/api/register/', this.registerFormGroup.value)
+        .subscribe((res: any) => {
+          this.snackBar.open('New user registered ', null, {
+            duration: 2000,
+          });
+          this.ngOnInit();
+        }, (error) => {
+          let msg = 'Registration failed! ';
 
-        if(error.error.username !== undefined) msg += '\n' + error.error.username;
-        if(error.error.email !== undefined) msg += '\n' + error.error.email;
-        if(error.error.password !== undefined) msg += '\n' + error.error.password;
+          if (error !== undefined && error.error !== undefined) {
+            if (error.error.username !== undefined) {
+              msg += '\n' + error.error.username;
+            }
+            if (error.error.email !== undefined) {
+              msg += '\n' + error.error.email;
+            }
+            if (error.error.password !== undefined) {
+              msg += '\n' + error.error.password;
+            }
+          } else {
+            msg += 'Unknown Error!';
+          }
 
-        this._snackBar.open(msg, null, {
-          duration: 2000,
+          this.snackBar.open(msg, null, {
+            duration: 2000,
+          });
         });
-      });
+    }
   }
 
+  emailValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const validEmail = emailRegex.test(control.value) || /space/.test(control.value);
+      return validEmail ? null : {'invalidEmail': {value: control.value}};
+    };
+  }
 }
